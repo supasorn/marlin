3f243f07d81bf5a95c5a8cde2d9a2fc4db973164
supasorn <supasorn@gmail.com>
2016-09-19 00:45:28 -0700

    revert software endstop

--- Configuration.h
+++ Configuration.h
@@ -617,7 +617,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the lo
 // Travel limits after homing (units are in mm)
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
-#define Z_MIN_POS -2
+#define Z_MIN_POS 0
 #define X_MAX_POS 200
 #define Y_MAX_POS 200
 #define Z_MAX_POS 200
